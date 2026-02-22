d895011b7321e8f68268ae5d8aaf5c493df3e800
SAI-KIRAN-2095 <saikiranpalakonda50@gmail.com>
2026-02-22 16:20:19 +0530

    Fix login URL in Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `https://mern-stack-complete-login-singup-oz.vercel.app/auth/login`;
+            const url = `https://mern-stack-complete-login-singup.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
